91a980684c8d9ad51dbe9797c5678c4386d587e4
finance215 <dreamledge@gmail.com>
2026-03-26 01:21:09 -0400

    Change build output to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/dreamledge/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   server: {
     host: true,
     port: 5173
