70b28d00c3f59f3380fb600259f5d87ce2ec18ca
finance215 <dreamledge@gmail.com>
2026-03-26 22:56:13 -0400

    Fix Vercel - hardcoded base /

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  base: isProduction ? '/' : '/dreamledge/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist'
